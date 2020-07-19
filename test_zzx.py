6b8744cbfe62d7d563a92bb598bb4c6ff5d3f7a5
StellaZhou1 <596832926@qq.com>
2020-07-19 14:56:28 +0800

    uploading file: get address, need to be finished by calling read_file function

--- test_zzx.py
+++ test_zzx.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilenames
+import tkinter as tk
+import util
 
 
 def read_file(image_path):
@@ -9,6 +11,7 @@ def read_file(image_path):
         f = open(image_path, 'rb')
         return f.read()
     except:
+        print('image path', image_path)
         print('read image file fail')
         return None
     finally:
@@ -21,23 +24,44 @@ def upload_callback():
     # selectPath()
 
 
-def selectPath():
-    path = askopenfilenames(initialdir='C:\\Windows',filetypes=[("all","*.*")])
+def select_path():
+    path = askopenfilenames(filetypes=[("all", "*.*")])
     pathname.set(path)
-    print(path)
     return path
 
-#TODO 调用read_file函数读入图片 完成upload
-file_content = read_file("D:/Desktop/pic.jpeg")
+
+def upload():
+    fns = root.tk.splitlist(pathname.get())
+    # print(repr(fns))
+    count = 0
+    print(fns)
+    for picName in fns:
+        if count == 0:
+            picName = picName[1:]
+            # print("start",picName)
+        if count == len(fns) - 1:
+            picName = picName[:-1]
+            # print("end",picName)
+        if count == len(fns) - 1 and len(fns) != 1:
+            result = util.ocr_general_basic(picName[1:-1])
+            print(result)
+        else:
+            result = util.ocr_general_basic(picName[1:-2])
+            count = count + 1
+            print(result)
+
+
 
 root = Tk()  # 创建窗口对象的背景色
-
-upload_btn = Button(root, text="上传图片", command=selectPath)
-upload_btn.pack()
 pathname = StringVar()
-
+choose_btn = Button(root, text="选择图片", command=select_path)
+choose_btn.pack()
 path_entry = Entry(root, textvariable=pathname)
 path_entry.pack(side=RIGHT)
 
+upload_btn = Button(root, text="上传图片", command=upload)
+upload_btn.pack()
+
 root.mainloop()
 
+# print(pathname.get())

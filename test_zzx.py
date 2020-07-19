79a0c1d7cf820b9d94dcd64092a7be3fd99fab79
StellaZhou1 <596832926@qq.com>
2020-07-19 16:23:34 +0800

    uploading file finished need to implement ui and database operations

--- test_zzx.py
+++ test_zzx.py
@@ -19,11 +19,6 @@ def read_file(image_path):
             f.close()
 
 
-def upload_callback():
-    messagebox.showinfo("Hello Python", "Hello Runoob")
-    # selectPath()
-
-
 def select_path():
     path = askopenfilenames(filetypes=[("all", "*.*")])
     pathname.set(path)
@@ -34,7 +29,6 @@ def upload():
     fns = root.tk.splitlist(pathname.get())
     # print(repr(fns))
     count = 0
-    print(fns)
     for picName in fns:
         if count == 0:
             picName = picName[1:]
@@ -43,17 +37,39 @@ def upload():
             picName = picName[:-1]
             # print("end",picName)
         if count == len(fns) - 1 and len(fns) != 1:
-            result = util.ocr_general_basic(picName[1:-1])
+            if ButtonList.get() == 0:
+                result = util.ocr_general_basic(picName[1:-1])
+            elif ButtonList.get() == 1:
+                result = util.ocr_business_card(picName[1:-1])
+            elif ButtonList.get() == 2:
+                result = util.ocr_business_license(picName[1:-1])
             print(result)
+            # TODO store result into system
         else:
-            result = util.ocr_general_basic(picName[1:-2])
-            count = count + 1
+            if ButtonList.get() == 0:
+                result = util.ocr_general_basic(picName[1:-2])
+            elif ButtonList.get() == 1:
+                result = util.ocr_business_card(picName[1:-2])
+            elif ButtonList.get() == 2:
+                result = util.ocr_business_license(picName[1:-2])
             print(result)
+        count = count + 1
 
 
-
-root = Tk()  # 创建窗口对象的背景色
+root = Tk()
 pathname = StringVar()
+
+ButtonList = IntVar()  # IntVar 是tkinter的一个类，可以管理单选按钮
+
+r1 = Radiobutton(root, variable=ButtonList, value=0, text="普通文本")
+r2 = Radiobutton(root, variable=ButtonList, value=1, text="名片")
+r3 = Radiobutton(root, variable=ButtonList, value=2, text="执照")
+# variable=从属的“管理类” value=索引/ID
+ButtonList.set(0)
+r1.pack()
+r2.pack()
+r3.pack()
+
 choose_btn = Button(root, text="选择图片", command=select_path)
 choose_btn.pack()
 path_entry = Entry(root, textvariable=pathname)
@@ -63,5 +79,3 @@ upload_btn = Button(root, text="上传图片", command=upload)
 upload_btn.pack()
 
 root.mainloop()
-
-# print(pathname.get())

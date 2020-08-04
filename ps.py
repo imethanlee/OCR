d11b1d60e8b3fde1da7c020ac0cf448af9195550
Ethan Lee <Howisitgoing@yeah.net>
2020-08-04 14:02:46 +0800

    1. 添加了PS功能，支持旋转与裁剪

--- ps.py
+++ ps.py
@@ -1,42 +1,63 @@
 import cv2
 
 
-global img
-global point1, point2
-
-
 class PS:
-    # TODO: 裁剪以及旋转的适配
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.img = cv2.imread(None)
+        self.rotated_img = cv2.imread(None)
+        self.point1 = []
+        self.point2 = []
+        self.curr_angle = 0
+        self.origin_x = 0
+
     def on_mouse(self, event, x, y, flags, param):
-        global img, point1, point2
-        img2 = img.copy()
+        img2 = self.rotated_img.copy()
         if event == cv2.EVENT_LBUTTONDOWN:  # 左键点击
-            point1 = (x, y)
-            cv2.circle(img2, point1, 10, (0, 255, 0), 5)
-            cv2.imshow('image', img2)
+            self.point1 = (x, y)
+            cv2.circle(img2, self.point1, 10, (0, 255, 0), 5)
+            cv2.imshow('PS', img2)
         elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):  # 按住左键拖曳
-            cv2.rectangle(img2, point1, (x, y), (255, 0, 0), 5)
-            cv2.imshow('image', img2)
+            cv2.rectangle(img2, self.point1, (x, y), (255, 0, 0), 5)
+            cv2.imshow('PS', img2)
         elif event == cv2.EVENT_LBUTTONUP:  # 左键释放
-            point2 = (x, y)
-            cv2.rectangle(img2, point1, point2, (0, 0, 255), 5)
-            cv2.imshow('image', img2)
-            min_x = min(point1[0], point2[0])
-            min_y = min(point1[1], point2[1])
-            width = abs(point1[0] - point2[0])
-            height = abs(point1[1] - point2[1])
-            cut_img = img[min_y:min_y + height, min_x:min_x + width]
-            # cv2.imwrite('haha.jpg', cut_img)
+            self.point2 = (x, y)
+            cv2.rectangle(img2, self.point1, self.point2, (0, 0, 255), 5)
+            cv2.imshow('PS', img2)
+            min_x = min(self.point1[0], self.point2[0])
+            min_y = min(self.point1[1], self.point2[1])
+            width = abs(self.point1[0] - self.point2[0])
+            height = abs(self.point1[1] - self.point2[1])
+            cut_img = self.rotated_img[min_y:min_y + height, min_x:min_x + width]
+            cv2.imwrite("default.jpg", cut_img)
+            cv2.imwrite(self.file_name, cut_img)
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            self.origin_x = x
+        elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_RBUTTON):
+            # 获取旋转角度
+            angle = self.get_angle(self.origin_x, x)
+            rows, cols, channels = self.img.shape
+            rotate = cv2.getRotationMatrix2D((cols * 0.5, rows * 0.5), self.curr_angle + angle, 1)
+            self.rotated_img = cv2.warpAffine(self.img, rotate, (cols, rows))
+            cv2.imshow('PS', self.rotated_img)
+        elif event == cv2.EVENT_RBUTTONUP:
+            self.curr_angle = self.curr_angle + self.get_angle(self.origin_x, x)
 
-    def main(self):
-        global img
-        img = cv2.imread('./test_case/bankcard.jpg')
-        cv2.namedWindow('image')
-        cv2.setMouseCallback('image', self.on_mouse)
-        cv2.imshow('image', img)
-        cv2.waitKey(0)
+    def run(self):
+        self.img = cv2.imread(self.file_name)
+        self.rotated_img = self.img
+        cv2.namedWindow('PS')
+        cv2.setMouseCallback('PS', self.on_mouse)
+        cv2.imshow('PS', self.img)
+        cv2.waitKey()
+
+    @staticmethod
+    def get_angle(old, new):
+        return 0.15 * (old - new)
 
 
-ps = PS()
-ps.main()
+ps = PS("./test_case/example.jpg")
+ps.run()
+
+
 
